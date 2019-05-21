a0bdfb2c2e1122a1974c754fe4139db668c4cbe4
Sergey Glushakov <sergey_glushakov_web@mail.ru>
2019-05-21 13:20:36 +0300

    **

--- smartgrid.js
+++ smartgrid.js
@@ -2,17 +2,18 @@ const smartgrid = require('smart-grid');
 
 /* It's principal settings in smart grid project */
 var settings = {
+    filename: "_smart-grid",
     outputStyle: 'scss', /* less || scss || sass || styl */
     columns: 12, /* number of grid columns */
     offset: '30px', /* gutter width px || % || rem */
     mobileFirst: false, /* mobileFirst ? 'min-width' : 'max-width' */
     container: {
-        maxWidth: '1100px', /* max-width оn very large screen */
-        fields: '30px' /* side fields */
+        maxWidth: '1600px', /* max-width оn very large screen */
+        fields: '15px' /* side fields */
     },
     breakPoints: {
         lg: {
-            width: '1100px', /* -> @media (max-width: 1100px) */
+            width: '1200px', /* -> @media (max-width: 1100px) */
         },
         md: {
             width: '991.99px'
